367e181434bfb466525617aea35645b0e2df88a3
Andrii <andrii.kondratovich@gmail.com>
2025-02-22 11:32:52 +0200

    add: add players to game on startedGame page

--- server_socket_io.js
+++ server_socket_io.js
@@ -9,15 +9,15 @@ const io = new Server(httpServer, { cors: { origin: "*" } });
 
 // Use Stream of MongoDB
 // Tracking changes the Game-collection in mongodb
-const changeStream = Game.watch();
-changeStream.on("change", change => {
-  console.log("Зміни в БД");
+// const changeStream = Game.watch();
+// changeStream.on("change", change => {
+//   console.log("Зміни в БД");
 
-  // Надсилаємо подію клієнтам лише при видаленні
-  if (change.operationType === "delete") {
-    io.emit("dbUpdateGamesColl", change);
-  }
-});
+//   // Надсилаємо подію клієнтам лише при видаленні
+//   if (change.operationType === "delete") {
+//     io.emit("dbUpdateGamesColl", change);
+//   }
+// });
 
 io.on("connection", socket => {
   console.log(`User connected: ${socket.id}`);
@@ -43,14 +43,7 @@ io.on("connection", socket => {
       if (!game) return socket.emit("error", { message: "Game not found" });
 
       const isPlayerExists = game.players.some(p => {
-        return p._id.toString() === player._id.toString(); // convert object to string and compare
-
-        // p._id.equals(player._id); // compare objects of Schema.Types.ObjectId
-        // p.userId === player._id; // compare strings
-
-        // convert string to object and compare:
-        // import { Schema, model } from "mongoose";
-        // p._id.equals(new mongoose.Types.ObjectId(player._id));
+        return p._id.toString() === player._id.toString();
       });
 
       // if game not started only host can start it
@@ -69,9 +62,16 @@ io.on("connection", socket => {
 
       game.players.push(player);
       await game.save();
-
       socket.join(gameId);
-      io.to(gameId).emit("updateGame", game);
+
+      // Update game for all players:
+      io.emit("updateGame", game);
+
+      // To all players in room "gameId" for message and redirect
+      io.to(gameId).emit("playerJoined", {
+        game,
+        message: `Player ${player.name.toUpperCase()} joined to this game`,
+      });
     } catch (err) {
       console.error("Error processing game action:", err);
       emitError("Server error");
@@ -79,12 +79,18 @@ io.on("connection", socket => {
   };
 
   const handleDeleteGame = async gameId => {
+    console.log("gameId:::", gameId);
     try {
       const deletedGame = await Game.findByIdAndDelete(gameId);
 
-      // io.emit("currentGameWasDeleted", deletedGame);
+      if (!deletedGame)
+        return io.to(gameId).emit("error", {
+          message: "Server error: cannot to delete game",
+        });
+
+      io.emit("currentGameWasDeleted", deletedGame);
     } catch (err) {
-      console.error("Error creating game:", err);
+      console.error("Error deleting game:", err);
       emitError("Server error");
     }
   };
